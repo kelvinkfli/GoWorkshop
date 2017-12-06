0feaab6d122ff91046b2b45d24f11fc6dd8dff78
Kelvin <kelvinkfli@gmail.com>
2017-12-06 16:17:03 -0500

    Remove comment

--- 6. structs/main.go
+++ 6. structs/main.go
@@ -1,8 +1,4 @@
 package main
 
-// Sometimes, primitive types aren't enough to communicate what we want to do with a function or application.
-// Go's answer to this problem is the struct.  It can be seen as a custom type that groups other typed fields
-// together.
-
 // Lets make a struct that represents a "square".
 func main() {}
